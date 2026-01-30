a1a97a6f65e1541dbb493ac44b858aa3c3fbe0ea
sleepyMS <minseok011017@gmail.com>
2026-01-30 20:40:00 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-for i in range(0):
-    print(i)
+import math
+
+def is_prime_optimized(n):
+    if n < 2: return False
+    # 2부터 n의 제곱근까지만 확인
+    for i in range(2, int(math.sqrt(n)) + 1):
+        print(i)
+        if n % i == 0:
+            return False
+    return True
+
+
+print(is_prime_optimized(2))
