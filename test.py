5095c44474aa378a7cce8056f45aab5ec2a63264
sleepyMS <minseok011017@gmail.com>
2026-01-31 05:51:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-import math
-
-def is_prime_optimized(n):
-    if n < 2: return False
-    # 2부터 n의 제곱근까지만 확인
-    for i in range(2, int(math.sqrt(n)) + 1):
-        print(i)
-        if n % i == 0:
-            return False
-    return True
-
-
-print(is_prime_optimized(2))
+a = [1,2,3]
+print(a[-4%3])
