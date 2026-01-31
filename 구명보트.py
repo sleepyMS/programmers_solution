3c1dafe6377e306bb2404fc0b6c0a326a6c0b470
sleepyMS <minseok011017@gmail.com>
2026-01-31 18:10:23 +0900

    Create 구명보트.py

---
+++ 구명보트.py
@@ -0,0 +1,13 @@
+def solution(people, limit):
+    answer = len(people)
+    people.sort()
+    people_re = sorted(people, reverse=True)
+    tmp = 0
+    answer_tmp = 0
+    for i, p in enumerate(people_re):
+        if p + people[tmp] <= limit and len(people)-i-1 != tmp:
+            tmp += 1
+            answer_tmp += 1
+            
+    return answer - answer_tmp // 2
+                
