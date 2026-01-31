4c602e6413ab555e20e328b7e10923f2e2163730
sleepyMS <minseok011017@gmail.com>
2026-01-31 22:15:52 +0900

    Update 등굣길.py

--- 등굣길.py
+++ 등굣길.py
@@ -1,29 +1,10 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/42898
-
-def solution(m, n, puddles):    
-    root = [[-1 for i in range(m)] for j in range(n)]
+def solution(m, n, puddles):
+    graph = [[0] * (m+1) for _ in range(n+1)]
+    graph[1][1] = 1
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if [j, i] not in puddles and graph[i][j] == 0:
+                graph[i][j] = graph[i-1][j] + graph[i][j-1]
+                
+    return graph[n][m] % 1000000007
     
-    for i in range(m):
-        if [i+1,1] in puddles:
-            for j in range(i,m):
-                root[0][j] = 0
-            break
-        else:
-            root[0][i] = 1
-    
-    for i in range(n):
-        if [1,i+1] in puddles:
-            for j in range(i,n):
-                root[j][0] = 0
-            break
-        else:
-            root[i][0] = 1
-            
-    for i in range(1, n):
-        for j in range(1, m):
-            if [j+1,i+1] in puddles:
-                root[i][j] = 0
-            else:
-                root[i][j] = root[i-1][j] + root[i][j-1]
-    
-    return root[n-1][m-1] % 1000000007
